4641bc65a68240a92491b7077790a40d54e3fd3f
moises valdez <shmoi@moisess-MBP.lan1>
2023-08-03 18:50:20 -0700

    Added new elements for this

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const element = document.getElementById('main');
 
 element.addEventListener('click', () => {alert('Hello World!')});
+
+// Path: main.js
+
+const element = document.getElementById('main');
+element.addEventListener('click', () => {alert('Hello World!')});
+// loop through the array and add a click event listener to each element
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', () => {alert('Hello World!')});
+}
