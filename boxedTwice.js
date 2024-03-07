b1f5471da5f59c17fe7640999962584ad5933eb5
moises valdez <moisesvaldez0559@gmail.com>
2024-03-07 14:57:05 -0800

    javascript interview question

---
+++ boxedTwice.js
@@ -0,0 +1,8 @@
+const array = [
+  { tweet: "hi", date: 2013 },
+  { tweet: "my", date: 2014 },
+  { tweet: "tweet", date: 2015 },
+]; // n^2
+
+console.log(array[0]);
+console.log(array[array.length - 1]);
