2c14a01d81320bf6c7593d401dc6195a27f544b3
moises valdez <moisesvaldez0559@gmail.com>
2024-03-24 09:12:38 -0700

    adding new features

---
+++ hashTable.js
@@ -0,0 +1,15 @@
+let user = {
+  age: 54,
+  name: "Moises",
+  magic: true,
+  scream: function () {
+    console.log("ahhhhh!");
+  },
+};
+
+console.log(user.age); // O(1)
+user.spell = "abra kadabra"; // O(1)
+console.log(user); // O(1)
+console.log(user.magic);
+
+// O(n/k) --> O(n)
