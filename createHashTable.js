f29f23842771074369e16cf83462c42e8dda9498
moises valdez <moisesvaldez0559@gmail.com>
2024-04-01 14:34:09 -0700

    key method to also inclue hash collision prevention

--- createHashTable.js
+++ createHashTable.js
@@ -37,14 +37,35 @@ class HashTable {
     return undefined;
   }
 
-  key() {
-    const keyArray = [];
+  // key() {
+  //   const keyArray = [];
+  //   for (let i = 0; i < this.data.length; i++) {
+  //     if (this.data[i]) {
+  //       keyArray.push(this.data[i][0][0]);
+  //     }
+  //   }
+  //   return keyArray;
+  // }
+  keys() {
+    if (!this.data.length) {
+      return undefined;
+    }
+    let result = [];
+    // loop through all the elements
     for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i]) {
-        keyArray.push(this.data[i][0][0]);
+      // if it's not an empty memory cell
+      if (this.data[i] && this.data[i].length) {
+        // but also loop through all the potential collisions
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0]);
+          }
+        } else {
+          result.push(this.data[i][0]);
+        }
       }
     }
-    return keyArray;
+    return result;
   }
 }
 
