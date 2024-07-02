6868c04ede10b2c287d7303790d7019651c7aab1
moises valdez <moisesvaldez0559@gmail.com>
2024-07-02 12:46:06 -0700

    learning about linked list

--- linkList.js
+++ linkList.js
@@ -1,5 +1,39 @@
-const obj1 = { name: "John" };
-const obj2 = obj1; // obj2 now holds a reference to the same object as obj1
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
 
-obj1.name = "Jane"; // Changes obj1.name to 'Jane'
-console.log(obj2.name); // Outputs: 'Jane' - obj2.name is also 'Jane'
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+
+console.log(myLinkedList);
