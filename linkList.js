d7ec603bc42b8e176aa55f403fd7ec3bf4f94b18
moises valdez <moisesvaldez0559@gmail.com>
2024-07-03 11:15:03 -0700

    added a printList function

--- linkList.js
+++ linkList.js
@@ -40,11 +40,23 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList() {
+    const array = [];
+
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.append(99);
 myLinkedList.prepend(1);
 
-console.log(myLinkedList);
+console.log(myLinkedList.printList());
