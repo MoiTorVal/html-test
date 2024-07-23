c5aee0c5df64b8f52e598fabc83dd3b005f684c5
moises valdez <moisesvaldez0559@gmail.com>
2024-07-22 18:13:20 -0700

    stack in linkedlist

--- stacks.js
+++ stacks.js
@@ -1,1 +1,54 @@
-// how javascript works
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    if (this.top === null) return null;
+    return this.top.value;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+  }
+
+  pop() {
+    if (this.length === 0) return null;
+
+    const value = this.top.value;
+    this.top = this.top.next;
+
+    if (this.length === 1) {
+      this.bottom = null;
+    }
+
+    this.length--;
+    return value;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+const myStack = new Stack();
+
+console.log(myStack.isEmpty());
